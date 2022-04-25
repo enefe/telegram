8aacce5d66e58429e268b07b4d5f789e4b0988fa
enefe815 <enefe815@yandex.ru>
2022-04-25 15:27:01 +0300

    feat: try-send-mes7

--- src/main.js
+++ src/main.js
@@ -112,13 +112,38 @@ pay.addEventListener('click', () => {
     let full = document.querySelector('.fullprice').textContent;
     Telegram.WebApp.close();
     Telegram.WebApp.sendData(full);
-    console.log(Telegram.WebApp.initData.user);
+    var res = fetch(telegramUrl + "/getUpdates");
+    console.log(res);
+    /* console.log(Telegram.WebApp.initData);
     console.log(Telegram.WebApp.initData.user.query_id);
 
-    /* bot.sendMessage(Telegram.WebApp.initData.user.id, '${full}'); */
+    bot.sendMessage(Telegram.WebApp.initData.user, '${full}'); */
 
     var token = "5359355956:AAEAMReleozRWWkMhGSA81MfiGS0ghEBPFo";
     var telegramUrl = "https://api.telegram.org/bot" + token;
+
+    let providerToken = '';
+
+    const getInvoice = (id) => {
+        const invoice = {
+            chat_id: id, // Уникальный идентификатор целевого чата или имя пользователя целевого канала
+            provider_token: providerToken, // токен выданный через бот @SberbankPaymentBot
+            start_parameter: 'get_access', //Уникальный параметр глубинных ссылок. Если оставить поле пустым, переадресованные копии отправленного сообщения будут иметь кнопку «Оплатить», позволяющую нескольким пользователям производить оплату непосредственно из пересылаемого сообщения, используя один и тот же счет. Если не пусто, перенаправленные копии отправленного сообщения будут иметь кнопку URL с глубокой ссылкой на бота (вместо кнопки оплаты) со значением, используемым в качестве начального параметра.
+            title: 'Итого к оплате:', // Название продукта, 1-32 символа
+            description: 'тест', // Описание продукта, 1-255 знаков
+            currency: 'RUB', // Трехбуквенный код валюты ISO 4217
+            prices: [{ label: 'Invoice Title', amount: 100 * 100 }], // Разбивка цен, сериализованный список компонентов в формате JSON 100 копеек * 100 = 100 рублей
+            photo_url: '/images/icon.png', // URL фотографии товара для счета-фактуры. Это может быть фотография товара или рекламное изображение услуги. Людям больше нравится, когда они видят, за что платят.
+            photo_width: 500, // Ширина фото
+            photo_height: 281, // Длина фото
+            payload: { // Полезные данные счета-фактуры, определенные ботом, 1–128 байт. Это не будет отображаться пользователю, используйте его для своих внутренних процессов.
+                unique_id: `${id}_${Number(new Date())}`,
+                provider_token: providerToken
+            }
+        }
+
+        return invoice
+    }
 
     /* function getChat_id() {
         var res = fetch(telegramUrl + "/getUpdates");
@@ -128,5 +153,7 @@ pay.addEventListener('click', () => {
     }
 
     getChat_id(); */
+
+
 
 })
